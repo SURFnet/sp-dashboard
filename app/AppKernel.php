cebcf6e28f08d0ea8b2c146f165ad842f882d161
Nic Wortel <nic@nicwortel.nl>
2017-04-25 15:02:39 +0200

    Remove sensio/framework-extra-bundle
    The bundle allows to configure controllers using annotations, but that will not be used in this project.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -14,7 +14,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
