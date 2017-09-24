71f8603436ddd3d692e91698d077bb3308c9c292
Joris Steyn <joris@ibuildings.nl>
2017-09-24 23:33:37 +0200

    Use sensio framework-extra-bundle for annotated routes
    This bundle provides the ability to use annotations to configure
    routes on controller actions.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -14,6 +14,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
