1917f0fbc654bfc17826d3d5f3682ef8edd68552
Joris Steyn <joris@ibuildings.nl>
2017-10-05 07:16:18 +0200

    Style attribute fields and disable when unchecked

---
+++ app/js/components/service_edit_attribute.js
@@ -0,0 +1,27 @@
+'use strict';
+
+$(document).ready(function() {
+    function enableOrDisableAttributeField() {
+        var rows = $(this)
+            .parents('.attribute-row-wrapper')
+            .find('.form-row');
+
+        if ($(this).is(':checked')) {
+            rows.removeClass('disabled');
+
+            var motivation = rows.find('input.motivation');
+            motivation.removeAttr('disabled');
+            motivation.val(motivation.data('old-value'));
+        } else {
+            rows.addClass('disabled');
+            rows.find('input.motivation').attr('disabled', 'disabled');
+
+            var motivation = rows.find('input.motivation');
+            motivation.data('old-value', motivation.val());
+            motivation.val('');
+        }
+    }
+
+    $('input.requested').each(enableOrDisableAttributeField);
+    $('input.requested').on('change', enableOrDisableAttributeField);
+});
