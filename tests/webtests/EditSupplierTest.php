5dbf1c7f1769970da2dbee0ccbd9fb694b65590d
Michiel Kodde <mkodde@ibuildings.nl>
2017-10-10 08:50:50 +0200

    Test the EditSupplier endpoint
    There is a bug in this not yet web-tested feature. The supplier edit
    page is not rendering because a non existing method is being called on
    a service.
    
    This test will cover this edit feature of the dashboard. Preventing
    future regression in this functionality.

---
+++ tests/webtests/EditSupplierTest.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Copyright 2017 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\ServiceProviderDashboard\Webtests;
+
+use Mockery as m;
+use Surfnet\ServiceProviderDashboard\Domain\Entity\Supplier;
+use Surfnet\ServiceProviderDashboard\Domain\Repository\SupplierRepository;
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class EditSupplierTest extends WebTestCase
+{
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * @var SupplierRepository
+     */
+    private $supplierRepository;
+
+    public function setUp()
+    {
+        $this->client = static::createClient();
+        $this->supplierRepository = $this->client->getContainer()->get('surfnet.dashboard.repository.supplier');
+        $this->supplierRepository->clear();
+
+        $supplier = m::mock(Supplier::class)->makePartial();
+        $supplier->setName('test1');
+        $supplier->setGuid('f1af6b9e-2546-4593-a57f-6ca34d2561e9');
+        $supplier->setTeamName('team-test');
+        $supplier->shouldReceive('getId')->andReturn(1);
+
+        $this->supplierRepository->save($supplier);
+
+        $this->client->getContainer()->get('surfnet.dashboard.service.admin_switcher')->setSelectedSupplier(1);
+    }
+
+    public function test_can_edit_existing_supplier()
+    {
+        $formData = [
+            'dashboard_bundle_edit_supplier_type' => [
+                'guid' => 'f1af6b9e-2546-4593-a57f-6ca34d2561e9',
+                'name' => 'The A Team',
+                'teamName' => 'team-a',
+            ]
+        ];
+
+        $crawler = $this->client->request('GET', '/supplier/edit');
+
+        $form = $crawler
+            ->selectButton('Save')
+            ->form();
+
+        $this->client->submit($form, $formData);
+
+        $supplier = $this->supplierRepository->findAll()[0];
+
+        $this->assertEquals('f1af6b9e-2546-4593-a57f-6ca34d2561e9', $supplier->getGuid());
+        $this->assertEquals('The A Team', $supplier->getName());
+        $this->assertEquals('team-a', $supplier->getTeamName());
+    }
+}
