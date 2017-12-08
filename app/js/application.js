7d5c3d37fb1145661431df3f6f7c6f5130ca82bd
Joris Steyn <joris@ibuildings.nl>
2017-12-08 09:35:23 +0100

    Implement tippy tooltips
    The previous css-only tooltips are now replaced with tippy.js-based
    tooltips. They adjust better to dynamic pieces of text.

--- app/js/application.js
+++ app/js/application.js
@@ -1,8 +1,8 @@
 'use strict';
 
 require('../../node_modules/jquery/dist/jquery.js');
-require('../../node_modules/tippy.js/dist/tippy.standalone.js');
 require('./components/service_switcher.js');
 require('./components/service_edit_attribute.js');
 require('./components/translation_ui.js');
+require('./components/tooltips.js');
 
---
+++ app/js/components/tooltips.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const tippy = require('../../../node_modules/tippy.js/dist/tippy.all.js');
+
+$(document).ready(
+    () => tippy(
+        '.help-button',
+       {
+           animation: 'scale',
+           arrow: true,
+           duration: 200,
+           placement: 'left',
+           theme: 'light'
+       }
+    )
+);
