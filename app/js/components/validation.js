632f6a444b3b534e24527f1649c2f5f152be2d84
Michiel Kodde <mkodde@ibuildings.nl>
2018-04-30 15:52:10 +0200

    Add motivation length warning
    When the motivation text is shorter than 10 characters, a warning is
    shown to the user informing a more lengthy motivation is requested.

--- app/js/components/validation.js
+++ app/js/components/validation.js
@@ -1,5 +1,6 @@
 import 'parsleyjs/src/parsley';
 
+// Configure Parsley
 const parsleyConfig = {
     errorsWrapper: '<ul></ul>',
     errorTemplate: '<li class="error"></li>',
@@ -15,5 +16,21 @@ const parsleyConfig = {
 const form = $('form[name="dashboard_bundle_entity_type"]');
 
 if (form.length) {
+    // Use parsley for url and email verification
     $(form).parsley(parsleyConfig);
+
+    // Custom validation is added to show the attribute motivation warnings
+    $('input.motivation').on('keyup', function () {
+
+        let parent = $(this).closest('.form-row');
+        let errorContainer = parent.parents('.form-row').find('.parsley-errors').first();
+
+        if ($(this).val().length <= 10) {
+            errorContainer.parent('.form-row').addClass('warning');
+            errorContainer.text('Please provide a more detailed motivation.');
+        } else {
+            errorContainer.parent('.form-row').removeClass('warning');
+            errorContainer.text('');
+        }
+    });
 }
