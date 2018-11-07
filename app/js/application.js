5f4df17b48d642f6c327501d40b5975ed19d9cda
Michiel Kodde <mkodde@ibuildings.nl>
2018-11-07 09:50:57 +0100

    Disable privacy questions service status
    The privacy questions service status radio group is disabled when
    privacy questions are not enabled for the service.

--- app/js/application.js
+++ app/js/application.js
@@ -2,6 +2,7 @@
 
 import 'jquery';
 import './components/service_switcher.js';
+import './components/service_status_interactions.js';
 import './components/validation.js';
 import './components/service_edit_attribute.js';
 import './components/translation_ui.js';
