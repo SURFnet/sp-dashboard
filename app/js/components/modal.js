5a16afa0bcce94e921e00b8964973fb6fe2d4fae
Michiel Kodde <mkodde@ibuildings.nl>
2018-11-28 09:50:39 +0100

    Typescriptify modal.js
    A type_definitions.ts file was added to the project. This file will
    be used to store home-made TS definitions which are not 'yet' available
    in the npm repositories.

--- app/js/components/modal.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-require('jquery-modal');
-
-$.modal.defaults = {
-    // Dont show the close button, a cancel button on the modal should perform this task
-    showClose: false
-};
---
+++ app/js/components/modal.ts
@@ -0,0 +1,7 @@
+import * as $ from 'jquery';
+import 'jquery-modal';
+
+$.modal.defaults = {
+  // Dont show the close button, a cancel button on the modal should perform this task
+  showClose: false,
+};
