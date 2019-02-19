ca5396a7cfb7e8d4200acd73ebfda171abdccbef
Michiel Kodde <mkodde@ibuildings.nl>
2019-02-19 08:49:13 +0100

    Aggregate Manage config in a value object
    The environment specific configuration for connecting to the manage
    instances, publication options and future other options are now
    encapsulated in a config aggregate.
    
    This service can then be fed to other services that need manage config
    information in order to function.

---
+++ src/Surfnet/ServiceProviderDashboard/Application/ViewObject/Manage/PublicationStatus.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Copyright 2019 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\ServiceProviderDashboard\Application\ViewObject\Manage;
+
+use Webmozart\Assert\Assert;
+
+class PublicationStatus
+{
+    /**
+     * @var string
+     */
+    private $createStatus;
+
+    /**
+     * @var string
+     */
+    private $updateStatus;
+
+    /**
+     * @param string $updateStatus
+     * @param string $createStatus
+     */
+    public function __construct($createStatus = '', $updateStatus = '')
+    {
+        Assert::stringNotEmpty($createStatus, 'Please set the create publication status in parameters.yml');
+        Assert::stringNotEmpty($updateStatus, 'Please set the update publication status in parameters.yml');
+
+        $this->createStatus = $createStatus;
+        $this->updateStatus = $updateStatus;
+    }
+
+    public function getCreateStatus()
+    {
+        return $this->createStatus;
+    }
+
+    public function getUpdateStatus()
+    {
+        return $this->updateStatus;
+    }
+}
