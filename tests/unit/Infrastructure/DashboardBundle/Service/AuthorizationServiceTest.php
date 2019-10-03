11f42c9b0e3114a90778c70bc5430649a577079e
Michiel Kodde <mkodde@ibuildings.nl>
2019-10-03 13:25:36 +0200

    Test if user is authorized to create OIDC entity
    This test is performed on the entity create action.

--- tests/unit/Infrastructure/DashboardBundle/Service/AuthorizationServiceTest.php
+++ tests/unit/Infrastructure/DashboardBundle/Service/AuthorizationServiceTest.php
@@ -24,6 +24,7 @@ use Mockery\MockInterface;
 use Surfnet\ServiceProviderDashboard\Application\Service\ServiceService;
 use Surfnet\ServiceProviderDashboard\Application\ViewObject\Manage\Config;
 use Surfnet\ServiceProviderDashboard\Domain\Entity\Service;
+use Surfnet\ServiceProviderDashboard\Domain\Service\OidcCreateEntityEnabledMarshaller;
 use Surfnet\ServiceProviderDashboard\Infrastructure\DashboardBundle\Exception\ManageConfigNotFoundException;
 use Surfnet\ServiceProviderDashboard\Infrastructure\DashboardBundle\Service\AuthorizationService;
 use Surfnet\ServiceProviderDashboard\Infrastructure\DashboardSamlBundle\Security\Authentication\Token\SamlToken;
@@ -51,6 +52,10 @@ class AuthorizationServiceTest extends MockeryTestCase
      * @var MockInterface&Config
      */
     private $manageConfigProd;
+    /**
+     * @var MockInterface&OidcCreateEntityEnabledMarshaller
+     */
+    private $marshaller;
 
     public function setUp()
     {
@@ -60,12 +65,15 @@ class AuthorizationServiceTest extends MockeryTestCase
         $this->manageConfigTest = m::mock(Config::class);
         $this->manageConfigProd = m::mock(Config::class);
 
+        $this->marshaller = m::mock(OidcCreateEntityEnabledMarshaller::class);
+
         $this->service = new AuthorizationService(
             $this->serviceService,
             $this->session,
             $this->tokenStorage,
             $this->manageConfigTest,
-            $this->manageConfigProd
+            $this->manageConfigProd,
+            $this->marshaller
         );
     }
 
@@ -181,5 +189,21 @@ class AuthorizationServiceTest extends MockeryTestCase
         $this->expectException(ManageConfigNotFoundException::class);
         $this->expectExceptionMessage('The manage configuration for environment "mumbojumbo" can not be found.');
         $this->service->isOidcngAllowed(m::mock(Service::class), 'mumbojumbo');
+    }
+
+    public function test_oidc_create_entity_allowed()
+    {
+        $this->marshaller
+            ->shouldReceive('allowed')
+            ->once()
+            ->andReturn(false);
+        $this->assertFalse($this->service->isOidcCreateEntityAllowed());
+
+        $this->marshaller
+            ->shouldReceive('allowed')
+            ->once()
+            ->andReturn(true);
+
+        $this->assertTrue($this->service->isOidcCreateEntityAllowed());
     }
 }
