a7a28dd3df8b96ba6b9d0f218e1ccb956be48d0b
Michiel Kodde <mkodde@ibuildings.nl>
2020-05-26 14:33:00 +0200

    Add the ManagePublishService
    Used to perform publication actions on the Manage environments

---
+++ tests/unit/Infrastructure/Manage/Service/ManagePublishServiceTest.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * Copyright 2020 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\ServiceProviderDashboard\Tests\Unit\Infrastructure\Manage\Service;
+
+use Mockery as m;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery\Mock;
+use Surfnet\ServiceProviderDashboard\Domain\Entity\Entity;
+use Surfnet\ServiceProviderDashboard\Infrastructure\Manage\Client\PublishEntityClient;
+use Surfnet\ServiceProviderDashboard\Infrastructure\Manage\Exception\InvalidArgumentException;
+use Surfnet\ServiceProviderDashboard\Infrastructure\Manage\Service\ManagePublishService;
+
+class ManagePublishServiceTest extends MockeryTestCase
+{
+    private $publishService;
+    /**
+     * @var Mock&PublishEntityClient
+     */
+    private $testClient;
+    /**
+     * @var Mock&PublishEntityClient
+     */
+    private $productionClient;
+
+    protected function setUp(): void
+    {
+        $this->testClient = m::mock(PublishEntityClient::class);
+        $this->productionClient = m::mock(PublishEntityClient::class);
+        $this->publishService = new ManagePublishService($this->testClient, $this->productionClient);
+    }
+
+    public function test_calling_publish_on_test_client()
+    {
+        $entity = m::mock(Entity::class);
+        $this->testClient
+            ->shouldReceive('publish')
+            ->with($entity)
+            ->once();
+
+        $this->publishService
+            ->publish('test', $entity);
+    }
+
+    public function test_calling_push_to_production()
+    {
+        $this->productionClient
+            ->shouldReceive('pushMetadata')
+            ->once();
+
+        $this->publishService
+            ->pushMetadata('production');
+    }
+
+    public function test_unknown_environment()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Please set the publication mode to one of these environments (test, production)');
+        $this->publishService->pushMetadata('ahkh-morpork');
+    }
+}
