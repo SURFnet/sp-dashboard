3a413de5c1cba00e1b24a17248ab53145baae73c
Koen Cornelis <kcornelis@ibuildings.nl>
2021-05-26 09:57:52 +0200

    Add unversioned cypress files

---
+++ cypress/plugins/index.js
@@ -0,0 +1,31 @@
+const htmlvalidate = require('cypress-html-validate/dist/plugin');
+
+module.exports = (on, config) => {
+    htmlvalidate.install(on, {
+        "rules": {
+            "prefer-button": "off",
+            "prefer-native-element": [ "error", {
+                "exclude": [ "button" ],
+            }],
+            "require-sri": [ "error", {
+                "target": "crossorigin",
+            }],
+        },
+    });
+
+    // debug a11y in ci
+    on('task', {
+          log(message) {
+                console.log(message);
+
+                return null;
+          },
+          table(message) {
+                console.table(message);
+
+                return null;
+          }
+      });
+
+    return config;
+};
---
+++ cypress/support/index.js
@@ -0,0 +1,8 @@
+// ***********************************************************
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+// ***********************************************************
+
+import './commands';
+import 'cypress-axe';
+import 'cypress-html-validate/dist/commands';
