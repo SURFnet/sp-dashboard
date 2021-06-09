b55769d760d183d95815244b1385c056998fed1b
Koen Cornelis <kcornelis@ibuildings.nl>
2021-06-09 16:35:36 +0200

    Write commands for login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,52 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('fillUsername', (username = 'John') => {
+    cy.get('#username').type(username);
+});
+
+Cypress.Commands.add('fillPassword', (pass = 'Doe') => {
+    cy.get('#password').type(pass);
+});
+
+Cypress.Commands.add('addMemberRole', (isMemberOf = 'eddy-wally') => {
+    cy.get('#add-attribute').select('urn:mace:dir:attribute-def:isMemberOf');
+    cy.get('input[name="urn:mace:dir:attribute-def:isMemberOf"]').type(isMemberOf);
+});
+
+Cypress.Commands.add('submitLoginForms', () => {
+    cy.get('.login-form').submit();
+    cy.get('body').then((body) => {
+        const isConsentPage = body.find('#accept').length;
+        if (isConsentPage) {
+            cy.get('#accept').submit();
+        }
+    });
+})
+
+Cypress.Commands.add('removeSFToolbar', () => {
+    cy.get('body').then((body) => {
+        const hasToolbar = body.find('.sf-toolbar').length;
+        if (hasToolbar) {
+            body.find('.sf-toolbar').remove();
+        }
+    });
+})
+
+Cypress.Commands.add('login', (username = 'John', pass = 'Doe', submit = true) => {
+    cy.visit('https://spdashboard.vm.openconext.org');
+    cy.fillUsername(username);
+    cy.fillPassword(pass);
+    if (submit) {
+        cy.submitLoginForms();
+    }
+});
+
+Cypress.Commands.add('loginWithMemberRole', (username = 'John', pass = 'Doe', isMemberOf = 'eddy-wally') => {
+    cy.login(username, pass, false);
+    cy.addMemberRole(isMemberOf);
+    cy.submitLoginForms().then(() => {
+        cy.removeSFToolbar();
+    });
+});
