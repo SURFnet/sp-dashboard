fd916c0ddd99c620fc7fae850b229c7536c77591
Koen Cornelis <kcornelis@ibuildings.nl>
2021-06-10 20:56:15 +0200

    Add a11y tests for service overview

---
+++ cypress/integration/a11y/dashboard.a11y.spec.js
@@ -0,0 +1,19 @@
+import {terminalLog} from '../../functions/terminalLog';
+
+context('SPD verify a11y of dashboard', () => {
+    beforeEach(() => {
+        cy.loginWithMemberRole();
+    });
+
+    it('contains no a11y problems on load', () => {
+        cy.injectAxe();
+        cy.checkA11y(null, null, terminalLog);
+    });
+
+    it('contains no html errors', () => {
+        cy.wait(300).then(() => {
+            cy.removeSFToolbar();
+            cy.htmlvalidate();
+        });
+    });
+});
