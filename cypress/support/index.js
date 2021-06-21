7c55398eac949fb0fdacaa8e8a52dd655301a9be
Koen Cornelis <kcornelis@ibuildings.nl>
2021-06-21 22:00:19 +0200

    Add remaining tests

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -4,15 +4,21 @@
 // ***********************************************************
 
 import './commands';
+import './commands/atttributes';
+import './commands/createEntity';
+import './commands/entityActions';
+import './commands/login';
+import './commands/manage';
+
 import 'cypress-axe';
 import 'cypress-html-validate/dist/commands';
 
 Cypress.on('uncaught:exception', (err, runnable) => {
-  // we expect a parsley library error with message 'parsley'
-  // and don't want to fail the test so we return false
-  if (err.message.includes('parsley')) {
-    return false
-  }
-  // we still want to ensure there are no other unexpected
-  // errors, so we let them fail the test
+    // we expect a parsley library error with message 'parsley'
+    // and don't want to fail the test so we return false
+    if (err.message.includes('parsley')) {
+        return false;
+    }
+    // we still want to ensure there are no other unexpected
+    // errors, so we let them fail the test
 })
