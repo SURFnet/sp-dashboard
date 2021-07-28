0f5017257d6870f7808c7d54cfacee128204f703
Bart Geesink <bart.geesink@surf.nl>
2021-07-28 11:11:01 +0200

    Cypress test GitHub action: Wait until the dashboard is reacheable

--- docker/conf/prep_oc.sh
+++ docker/conf/prep_oc.sh
@@ -23,7 +23,7 @@ curl  -H 'Content-Type: application/json' -u sysadmin:$pass -d@/usr/local/etc/sa
 curl  -u sysadmin:$pass -XGET https://manage.vm.openconext.org/manage/api/internal/push
 
 # Set the Mujina Guest qualifier to "none"
-mujina_entity_id=$(curl -s -H  'Content-Type: application/json' -u sysadmin:$pass  -X POST -d '{"ALL_ATTRIBUTES":true, "entityid":"http://mock-idp"}' 'https://manage.vm.openconext.org/manage/api/internal/search/saml20_idp'| python2 -c 'import sys, json; print json.load(sys.stdin)[0]["_id"]'
+mujina_entity_id=$(curl -s -H  'Content-Type: application/json' -u sysadmin:$pass  -X POST -d '{"ALL_ATTRIBUTES":true, "entityid":"http://mock-idp"}' 'https://manage.vm.openconext.org/manage/api/internal/search/saml20_idp'| python2 -c 'import sys, json; print json.load(sys.stdin)[0]["_id"]')
 curl -s -H 'Content-Type: application/json' -u sysadmin:$pass -X PUT -d '{"id": "'$mujina_entity_id'", "type": "saml20_idp", "pathUpdates": { "metaDataFields.coin:guest_qualifier": "None" }}' https://manage.vm.openconext.org/manage/api/internal/merge
 curl  -u sysadmin:$pass -XGET https://manage.vm.openconext.org/manage/api/internal/push
 
