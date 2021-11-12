23ba1ef239b00cb697d8f7e35e01165756680f49
Koen Cornelis <kcornelis@ibuildings.nl>
2021-11-12 15:07:54 +0100

    Filter out non-allowed chars from teamname
    The current filters for the teamname weren't sufficient.  As such they had to be adjust after clarification.  We now filter out the following:
    - any non-word character save for -
    - any diacritical character gets transformed to it's normal counterpart
    
    Additionally any spaces are transformed into a -
    
    See pivotal @ https://www.pivotaltracker.com/story/show/180244357

--- app/js/components/add_service.ts
+++ app/js/components/add_service.ts
@@ -7,10 +7,17 @@ $(() => {
     const name = document.getElementById('dashboard_bundle_service_type_general_name') as HTMLInputElement;
     const organizationEn = document.getElementById('dashboard_bundle_service_type_general_organizationNameEn') as HTMLInputElement;
     const setTeamName = () => {
-      // @ts-ignore
-      const serviceName = name.value.replaceAll(' ', '-').replaceAll("'", '').replaceAll('"', '');
-      // @ts-ignore
-      const organizationName = organizationEn.value.replaceAll(' ', '-').replaceAll("'", '').replaceAll('"', '');
+      const normalizeName = (nameToNormalize: string) => {
+        /**
+         * The below was modified from: https://stackoverflow.com/a/37511463/11339541
+         * We allow only word characters (=digits, letters &, underscores), spaces and minus signs
+         * We do not allow diacritics (letters with a special modifier, like é, è or ô for example)
+         */
+        // @ts-ignore
+        return nameToNormalize.normalize('NFD').replace(/\p{Diacritic}/gu, '').replace(/[^\W\u00a0\u0020\-]/gu, '').replace(/[\u00a0\u0020]/gu, '-');
+      };
+      const serviceName = normalizeName(name.value);
+      const organizationName = normalizeName(organizationEn.value);
       teamName.value = `spd_${organizationName}_${serviceName}`.toLowerCase();
     };
 
