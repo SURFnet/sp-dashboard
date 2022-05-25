783720461d3980d92bef7db186e2c3b06550c414
Vadim Schmitz <di3mpi3@gmail.com>
2022-05-25 15:08:30 +0200

    Replace Select dropown with a css dropdown
    To Use icons within the dropdowns and make the application styling more inline with other dropdowns the current select dropdowns have been replaced with a css styled dropdown.
    Due to another part of the application relying on the select dropdown
    the pull request quickly grew out of scope. Therefore all commits have
    been squashed.

--- app/js/components/delete_member.ts
+++ app/js/components/delete_member.ts
@@ -6,37 +6,39 @@ $(() => {
   const deleteLinks = document.querySelectorAll('.teams__deleteMemberLink');
 
   if (!!flashUl && !!deleteLinks) {
-    document.addEventListener('click', (event) => {
-      const target = event.target as HTMLSelectElement;
-      if (!target.matches('.teams__deleteMemberLink')) return;
-      event.preventDefault();
+    deleteLinks.forEach(item => {
+      item.addEventListener('mousedown', (event) => {
+        event.stopImmediatePropagation();
+        const target = event.target as HTMLSelectElement;
+        if (!target.matches('.teams__deleteMemberLink')) return;
 
-      const td = target.closest('td') as HTMLTableDataCellElement;
-      td.classList.add('loading');
+        const td = target.closest('td') as HTMLTableDataCellElement;
+        td.classList.add('loading');
 
-      const url = target.getAttribute('href') || '';
-      const closeText = flashUl.getAttribute('data-close') || 'Close';
-      const deleteMemberText = flashUl.getAttribute('data-deleteMember') || '';
-      const name = target.getAttribute('data-name') || '';
+        const url = target.getAttribute('href') || '';
+        const closeText = flashUl.getAttribute('data-close') || 'Close';
+        const deleteMemberText = flashUl.getAttribute('data-deleteMember') || '';
+        const name = target.getAttribute('data-name') || '';
 
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          if (data !== 'success') {
-            addFlashMessage(flashUl, data, 'error', closeText);
+        fetch(url)
+          .then(response => response.json())
+          .then(data => {
+            if (data !== 'success') {
+              addFlashMessage(flashUl, data, 'error', closeText);
+              td.classList.remove('loading');
+              return;
+            }
+
+            const message = deleteMemberText.replace('<name>', name);
+            addFlashMessage(flashUl, message, 'info', closeText);
             td.classList.remove('loading');
-            return;
-          }
-
-          const message = deleteMemberText.replace('<name>', name);
-          addFlashMessage(flashUl, message, 'info', closeText);
-          td.classList.remove('loading');
-          td.parentElement?.remove();
-        })
-        .catch(error => {
-          addFlashMessage(flashUl, error.message, 'error', closeText);
-          td.classList.remove('loading');
-        });
+            td.parentElement?.remove();
+          })
+          .catch(error => {
+            addFlashMessage(flashUl, error.message, 'error', closeText);
+            td.classList.remove('loading');
+          });
+      });
     });
   }
 });
