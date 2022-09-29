4dc042b35ffd5fd6deff99fa118eef1ff16f3b85
Sjaak Kallemein <sjaakkallemein@Sjaaks-MacBook-Pro-2.local>
2022-09-29 11:01:09 +0200

    Provide a service to fetch and store entity attribute properties
    see https://www.pivotaltracker.com/story/show/182220241

---
+++ src/Surfnet/ServiceProviderDashboard/Infrastructure/DashboardBundle/Repository/AttributeRepository.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2022 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\ServiceProviderDashboard\Infrastructure\DashboardBundle\Repository;
+
+use Surfnet\ServiceProviderDashboard\Infrastructure\DashboardBundle\Dto\Attribute;
+
+class AttributeRepository implements AttributeRepositoryInterface
+{
+    private $attributesFileLocation;
+    private $attributes = [];
+
+    public function __construct(string $attributesFileLocation)
+    {
+        $this->attributesFileLocation = $attributesFileLocation;
+    }
+
+    private function load(): array
+    {
+        return json_decode(
+            file_get_contents($this->attributesFileLocation),
+            true
+        );
+    }
+
+    private function getAttributes(): array
+    {
+        if (empty($this->attributes)) {
+            foreach ($this->load() as $rawAttribute) {
+                $this->attributes[] = Attribute::fromAttribute($rawAttribute);
+            }
+        }
+        return $this->attributes;
+    }
+
+    public function findAll(): array
+    {
+        return $this->getAttributes();
+    }
+}
---
+++ tests/unit/Infrastructure/DashboardBundle/Repository/AttributeRepositoryTest.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Copyright 2022 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\ServiceProviderDashboard\Tests\Unit\Infrastructure\DashboardBundle\Repository;
+
+use PHPUnit\Framework\TestCase;
+use Surfnet\ServiceProviderDashboard\Infrastructure\DashboardBundle\Repository\AttributeRepository;
+
+class AttributeRepositoryTest extends TestCase
+{
+    public function test_it_loads_attributes()
+    {
+        $attributeRepository = new AttributeRepository(__DIR__ . '/Fixtures/attributes.json');
+        $results = $attributeRepository->findAll();
+        $this->assertIsArray($results);
+        $this->assertCount(15, $results);
+    }
+
+    public function test_it_loads_empty_attributes()
+    {
+        $attributeRepository = new AttributeRepository(__DIR__ . '/Fixtures/attributes-empty.json');
+        $results = $attributeRepository->findAll();
+        $this->assertEmpty($results);
+    }
+
+    public function test_it_has_all_attributes()
+    {
+        $expectedAttributes = [
+            "displayName",
+            "affiliation",
+            "scopedaffiliation",
+            "emailAddress",
+            "commonName",
+            "organization",
+            "organizationType",
+            "organizationUnit",
+            "surName",
+            "givenName",
+            "entitlement",
+            "uid",
+            "principleName",
+            "preferredLanguage",
+            "personalCode",
+        ];
+        $attributeRepository = new AttributeRepository(__DIR__ . '/Fixtures/attributes.json');
+        $attributes = $attributeRepository->findAll();
+
+        foreach ($attributes as $attribute) {
+            $this->assertContains($attribute->id, $expectedAttributes);
+        }
+    }
+}
