ed7e6ecdcbce0607066742501e9d9926d95b423d
Sjaak Kallemein <sjaakkallemein@Sjaaks-MacBook-Pro-2.local>
2022-10-07 11:44:28 +0200

    Introduce attribute view object to hide internal complexity of attributes
    see: https://www.pivotaltracker.com/story/show/182220241 (Form Type changes)

---
+++ src/Surfnet/ServiceProviderDashboard/Application/ViewObject/Attribute.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * Copyright 2022 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+namespace Surfnet\ServiceProviderDashboard\Application\ViewObject;
+
+use Surfnet\ServiceProviderDashboard\Infrastructure\DashboardBundle\Dto\AttributeFormLanguage;
+
+class Attribute
+{
+    const ATTRIBUTE_NAME_SUFFIX = 'Attribute';
+
+    /**
+     * @var string
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $label;
+
+    /**
+     * @var string
+     */
+    private $info;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    private $urns = [];
+
+    public function __construct(
+        string $id,
+        string $label,
+        string $info,
+        string $name,
+        array $urns
+    ) {
+        $this->id = $id;
+        $this->label = $label;
+        $this->info = $info;
+        $this->name = $name;
+        $this->urns = $urns;
+    }
+
+    public static function fromAttribute(
+        \Surfnet\ServiceProviderDashboard\Infrastructure\DashboardBundle\Dto\Attribute $attribute,
+        AttributeFormLanguage $language
+    ): Attribute {
+
+        return new self(
+            $attribute->id,
+            $language->label,
+            $language->info,
+            $attribute->id . ATTRIBUTE::ATTRIBUTE_NAME_SUFFIX,
+            $attribute->urns
+        );
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): string
+    {
+        return $this->label;
+    }
+
+    public function getInfo(): string
+    {
+        return $this->info;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getUrns(): array
+    {
+        return $this->urns;
+    }
+}
