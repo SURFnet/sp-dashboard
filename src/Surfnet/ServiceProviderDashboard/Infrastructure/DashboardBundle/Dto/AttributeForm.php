cac39450a2d7209d75d5e9c93017ae87d69866b5
Sjaak Kallemein <sjaakkallemein@Sjaaks-MacBook-Pro-2.local>
2022-10-07 11:44:28 +0200

    Introduce attribute view object to hide internal complexity of attributes
    see: https://www.pivotaltracker.com/story/show/182220241 (Form Type changes)

---
+++ src/Surfnet/ServiceProviderDashboard/Infrastructure/DashboardBundle/Dto/AttributeForm.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2022 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\ServiceProviderDashboard\Infrastructure\DashboardBundle\Dto;
+
+class Form
+{
+    public $languages = [];
+
+    private function __construct(
+        array $languages
+    ) {
+        foreach ($languages as $language => $values) {
+            $this->languages[$language] = language::fromLanguage($values);
+        }
+    }
+
+    public static function fromForm(array $languages): ?Form
+    {
+        return new self(
+            $languages
+        );
+    }
+}
