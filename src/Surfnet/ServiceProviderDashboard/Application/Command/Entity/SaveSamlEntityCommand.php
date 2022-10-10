c78f31ee1d198b96100a79886c300d2a3b77f271
Sjaak Kallemein <sjaakkallemein@Sjaaks-MacBook-Pro-2.local>
2022-10-10 14:25:53 +0200

    Add assert callback on attributes

--- src/Surfnet/ServiceProviderDashboard/Application/Command/Entity/SaveSamlEntityCommand.php
+++ src/Surfnet/ServiceProviderDashboard/Application/Command/Entity/SaveSamlEntityCommand.php
@@ -25,6 +25,7 @@ use Surfnet\ServiceProviderDashboard\Domain\ValueObject\Attribute;
 use Surfnet\ServiceProviderDashboard\Domain\ValueObject\Contact;
 use Surfnet\ServiceProviderDashboard\Infrastructure\DashboardBundle\Validator\Constraints as SpDashboardAssert;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 /**
  * @SuppressWarnings(PHPMD.TooManyFields)
@@ -222,12 +223,41 @@ class SaveSamlEntityCommand implements SaveEntityCommandInterface
     {
     }
 
+    /**
+     * @Assert\Callback()
+     */
+    public function validate(ExecutionContextInterface $context, $payload)
+    {
+        foreach ($this->attributes as $name => $attribute) {
+            if (isset($attribute) && !($attribute instanceof Attribute)) {
+                $context->buildViolation(sprintf('entity.edit.attribute.invalid', $name))
+                    ->atPath('attribute')
+                    ->addViolation();
+            }
+        }
+    }
+
+    /**
+     * The magic getters and setters are consulted by the saml form builder.
+     * Another option would be to implement a dataMapper on the
+     * form or attribute container, but this might lead to needless complexity.
+     */
     public function __set(string $property, ?Attribute $value)
+    {
+        $this->setAttribute($property, $value);
+    }
+
+    public function __get(string $property): ?Attribute
+    {
+        return $this->getAttribute($property);
+    }
+
+    public function setAttribute(string $property, ?Attribute $value)
     {
         $this->attributes[$property] = $value;
     }
 
-    public function __get(string $property): ?Attribute
+    public function getAttribute(string $property): ?Attribute
     {
         if (array_key_exists($property, $this->attributes)) {
             return $this->attributes[$property];
@@ -235,15 +265,6 @@ class SaveSamlEntityCommand implements SaveEntityCommandInterface
         return null;
     }
 
-    public function setAttribute(string $property, ?Attribute $value)
-    {
-        $this->__set($property, $value);
-    }
-
-    public function getAttribute(string $property): ?Attribute
-    {
-        return $this->__get($property);
-    }
     /**
      * @param Service $service
      * @return SaveSamlEntityCommand
