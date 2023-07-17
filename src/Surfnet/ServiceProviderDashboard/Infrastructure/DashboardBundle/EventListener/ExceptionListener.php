33adca0a6232eb12bd90b035656c250452b88c8e
Michiel Kodde <mkodde@ibuildings.nl>
2023-07-17 11:46:30 +0200

    Display custom authnFailed error page
    In order to show the unknown service error page (when the team id is not
    matching up to any of the services kwown to SPD). I needed to tie in to
    the exception listener service. As SF would just render a generic 500
    page when the ServiceNotFound exception remained uncatched. The listener
    now checks if that exception is handled. If so, it renders the fancy
    error page.
    
    The authnFailed page was not yet displayed when authn fails for
    whatever reason. You would expect the exception listener would be used
    for this purpose, but the FailureHandler is used for this..

---
+++ src/Surfnet/ServiceProviderDashboard/Infrastructure/DashboardBundle/EventListener/ExceptionListener.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2023 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\ServiceProviderDashboard\Infrastructure\DashboardBundle\EventListener;
+
+use Psr\Log\LoggerInterface;
+use Surfnet\ServiceProviderDashboard\Infrastructure\DashboardSamlBundle\Security\Exception\UnknownServiceException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Twig\Environment;
+
+class ExceptionListener
+{
+    public function __construct(
+        private LoggerInterface $logger,
+        private Environment $templating,
+    ) {
+    }
+
+    public function __invoke(ExceptionEvent $event): void
+    {
+        // You get the exception object from the received event
+        $exception = $event->getThrowable();
+        if ($exception instanceof UnknownServiceException) {
+            $responseBody = $this->templating->render(
+                '@Twig/Exception/unknownService.html.twig',
+                ['teamNames' => $exception->getTeamNames()]
+            );
+
+            $event->setResponse(new Response($responseBody, Response::HTTP_UNAUTHORIZED));
+        }
+    }
+}
