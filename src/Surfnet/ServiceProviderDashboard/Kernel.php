1103cf6e0be6c47be69a7cf71ab9bc9f1076d9cd
Paul Rijke <paul@ibuildings.nl>
2024-01-17 16:24:05 +0100

    Add getProjectDir to Kernel

--- src/Surfnet/ServiceProviderDashboard/Kernel.php
+++ src/Surfnet/ServiceProviderDashboard/Kernel.php
@@ -26,4 +26,11 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    // see https://symfony.com/doc/current/reference/configuration/kernel.html#kernel-project-dir
+    public function getProjectDir(): string
+    {
+        return dirname(__DIR__);
+    }
+
 }
