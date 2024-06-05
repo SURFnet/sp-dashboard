bc37877f6d084808708e78bbc66d08e44c12270c
Bart Geesink <bart.geesink@surf.nl>
2024-06-05 12:46:24 +0200

    GHA: Test integration: Use devconf

--- docker/conf/cypress_import_cert.sh
+++ docker/conf/cypress_import_cert.sh
@@ -1,10 +1,10 @@
 # This will import the certificate offered by manage.dev.openconext.local into the local certificate store.
 
-until $(curl -k --output /dev/null --silent --head --fail https://manage.dev.openconext.local); do
-    printf '.'
-    sleep 1
+until $(curl -k --output /dev/null --silent --head --fail https://manage.dev.openconext.local/internal/health); do
+	printf '.'
+	sleep 1
 done
 
-echo QUIT | openssl s_client -connect manage.dev.openconext.local:443  | sed -ne '/BEGIN CERT/,/END CERT/p' > /usr/share/ca-certificates/openconext.pem
-echo openconext.pem >> /etc/ca-certificates.conf
+echo QUIT | openssl s_client -connect manage.dev.openconext.local:443 | sed -ne '/BEGIN CERT/,/END CERT/p' >/usr/share/ca-certificates/openconext.pem
+echo openconext.pem >>/etc/ca-certificates.conf
 update-ca-certificates
