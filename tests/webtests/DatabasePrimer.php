ed88bbd7be8c59f2637011e3cd0ec6f2e90704ef
Paul Rijke <paul@ibuildings.nl>
2024-06-27 14:34:13 +0200

    Use in memory DB

---
+++ tests/webtests/DatabasePrimer.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Copyright 2024 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Prime class and prime static method from
+// https://www.sitepoint.com/quick-tip-testing-symfony-apps-with-a-disposable-database/
+
+namespace Surfnet\ServiceProviderDashboard\Webtests;
+
+use Doctrine\ORM\Tools\SchemaTool;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class DatabasePrimer
+{
+    public static function prime(KernelInterface $kernel)
+    {
+        // Make sure we are in the test environment
+        if ('test' !== $kernel->getEnvironment()) {
+            throw new \LogicException('Primer must be executed in the test environment');
+        }
+
+        // Get the entity manager from the service container
+        $entityManager = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+        // Run the schema update tool using our entity metadata
+        $metadata = $entityManager->getMetadataFactory()->getAllMetadata();
+        $schemaTool = new SchemaTool($entityManager);
+        $schemaTool->updateSchema($metadata);
+        // If you are using the Doctrine Fixtures Bundle you could load these here
+    }
+
+}
