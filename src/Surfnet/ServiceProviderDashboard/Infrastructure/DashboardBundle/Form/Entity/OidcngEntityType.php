2c53fa2015f32029e65864253bce73cc16172eb3
Michiel Kodde <mkodde@ibuildings.nl>
2024-07-02 13:46:45 +0200

    Improve component Type of service UX
    - JS validation was added. This improves the rules regarding selecting
      the number of required TOS entries
    - The addition of the ;decorated; class on the choice attributes
      improved styling
    - html5 validation was added, the group is now `required`

--- src/Surfnet/ServiceProviderDashboard/Infrastructure/DashboardBundle/Form/Entity/OidcngEntityType.php
+++ src/Surfnet/ServiceProviderDashboard/Infrastructure/DashboardBundle/Form/Entity/OidcngEntityType.php
@@ -249,10 +249,15 @@ class OidcngEntityType extends AbstractType
                 'typeOfService',
                 ChoiceType::class,
                 [
-                    'required' => false,
+                    'required' => true,
                     'choices' => $this->typeOfServiceProvider->getTypesOfServiceChoices(),
                     'choice_value' => fn(TypeOfService $tos): string => $tos->typeEn,
                     'choice_label' => fn(TypeOfService $tos): string => $tos->typeEn,
+                    'choice_attr' => fn(): array => [
+                        'class' => 'decorated',
+                        'data-parsley-mincheck' => 1,
+                        'data-parsley-maxcheck' => 3,
+                    ],
                     'expanded' => true,
                     'multiple' => true,
                     'attr' => [
