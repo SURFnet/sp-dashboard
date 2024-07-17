f69373d2188ad40dd665101718e4569256d97452
Paul Rijke <paul@ibuildings.nl>
2024-07-17 16:20:52 +0200

    Correct docheader

--- src/Surfnet/ServiceProviderDashboard/Infrastructure/DashboardBundle/EventListener/AuthenticatedUserListener.php
+++ src/Surfnet/ServiceProviderDashboard/Infrastructure/DashboardBundle/EventListener/AuthenticatedUserListener.php
@@ -3,7 +3,7 @@
 declare(strict_types = 1);
 
 /**
- * Copyright 2016 SURFnet bv
+ * Copyright 2024 SURFnet B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
