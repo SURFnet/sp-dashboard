95205316a2b1994750db663c7b1d4001d1cbafe2
Johan Kromhout <jkromhout@ibuildings.nl>
2024-10-31 11:54:04 +0100

    Revert "Remove the `production_entities_enabled` field"
    This reverts commit a03baf49

--- src/Surfnet/ServiceProviderDashboard/Domain/Entity/Service.php
+++ src/Surfnet/ServiceProviderDashboard/Domain/Entity/Service.php
@@ -22,10 +22,13 @@ use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Surfnet\ServiceProviderDashboard\Domain\Entity\PrivacyQuestions;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Surfnet\ServiceProviderDashboard\Infrastructure\DashboardBundle\Repository\ServiceRepository;
 
 /**
+ * @package Surfnet\ServiceProviderDashboard\Entity
+ *
  * @SuppressWarnings(PHPMD.TooManyFields)
  * @SuppressWarnings(PHPMD.UnusedPrivateField) - the createdAt field is not used in code, but used by TPM's to check for
  *                                               the creation date of a service.
@@ -88,6 +91,9 @@ class Service
      */
     #[ORM\Column(length: 255)]
     private $teamName;
+
+    #[ORM\Column(type: 'boolean')]
+    private bool $productionEntitiesEnabled = false;
 
     #[ORM\Column(type: 'boolean')]
     private bool $privacyQuestionsEnabled = true;
@@ -192,6 +198,11 @@ class Service
         $this->name = $name;
     }
 
+    public function setProductionEntitiesEnabled(bool $enabled): void
+    {
+        $this->productionEntitiesEnabled = $enabled;
+    }
+
     public function setPrivacyQuestionsEnabled(bool $privacyQuestionsEnabled): void
     {
         $this->privacyQuestionsEnabled = $privacyQuestionsEnabled;
@@ -216,6 +227,11 @@ class Service
     public function setPrivacyQuestions(PrivacyQuestions $privacyQuestions): void
     {
         $this->privacyQuestions = $privacyQuestions;
+    }
+
+    public function isProductionEntitiesEnabled(): bool
+    {
+        return $this->productionEntitiesEnabled;
     }
 
     public function isPrivacyQuestionsEnabled(): bool
