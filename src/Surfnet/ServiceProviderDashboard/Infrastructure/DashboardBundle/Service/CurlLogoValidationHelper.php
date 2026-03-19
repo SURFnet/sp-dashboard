b448232b0c34c81b4c9d696f3116fd0f58b3f3f4
Kay Joosten <kay.joosten@dawn.tech>
2026-03-19 13:59:04 +0100

    chore: remove redundant CURLOPT_HEADER setting
    CURLOPT_HEADER defaults to 0, setting it explicitly is unnecessary.

--- src/Surfnet/ServiceProviderDashboard/Infrastructure/DashboardBundle/Service/CurlLogoValidationHelper.php
+++ src/Surfnet/ServiceProviderDashboard/Infrastructure/DashboardBundle/Service/CurlLogoValidationHelper.php
@@ -45,7 +45,6 @@ class CurlLogoValidationHelper implements LogoValidationHelperInterface
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_VERBOSE, 1);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
         curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 
